a1a9875304def734c3b35205a4ffaf4074297107
Taylor Halsted <taylor.halsted@gmail.com>
2016-11-01 16:31:51 -0400

    add board

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 $(document).ready(function(){
-console.log('JS Loaded.')
+  console.log('JavaScript loaded.')
+
+  var makeBoard = function() {
+    var $block = $('<div class="block"></div>');
+
+    for (var i = 0; i < 7; i++) {
+      var $picker = $('<div class="picker"></div>');
+      $picker.attr('data-column',i)
+      $('#board').append($picker)
+    }
+
+    for (var r = 0; r < 6; r++) {
+      for (var c = 0; c < 7; c++) {
+        var $block = $('<div class="block"></div>');
+        $block
+          .attr('data-column',c)
+          .attr('data-row',r)
+        $('#board').append($block)
+      }
+    }
+  }
 
 
-
+  makeBoard();
 })
